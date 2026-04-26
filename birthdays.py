a15833fd5515719193f0556ff7a1490706616a33
Aiday <903774@stud.unive.it>
2026-04-26 13:48:35 +0200

    commiting

--- birthdays.py
+++ birthdays.py
@@ -1,3 +1,8 @@
+"""
+This module provides a simple birthday dictionary and functions
+to display available names and retrieve a person's birthday.
+"""
+
 birthdays = {
     'Albert Einstein': '03/14/1879',
     'Benjamin Franklin': '01/17/1706',
@@ -6,34 +11,14 @@ birthdays = {
     'Rowan Atkinson': '01/6/1955'}
 
 def print_birthdays():
+    """Prints a welcome message and lists all the names in the birthday dictionary."""
     print('Welcome to the birthday dictionary. We know the birthdays of these people:')
     for name in birthdays:
         print(name)
 
 def return_birthday(name):
-    if name in birthdays:
-        print('{}\'s birthday is {}.'.format(name, birthdays[name]))
-    else:
-        print('Sadly, we don\'t have {}\'s birthday.'.format(name))
-
-"""
-Comment the module goal here.
-"""
-
-birthdays = {
-    'Albert Einstein': '03/14/1879',
-    'Benjamin Franklin': '01/17/1706',
-    'Ada Lovelace': '12/10/1815',
-    'Donald Trump': '06/14/1946',
-    'Rowan Atkinson': '01/6/1955'}
-
-def print_birthdays():
-    """Comment the function."""
-    print('Welcome to the birthday dictionary. We know the birthdays of these people')
-    for name in birthdays:
-        print(name)
-def return_birthday(name):
-    """Comment the function."""
+    """Prints the birthday of the given person if present in the dictionary,
+        otherwise informs the user that the birthday is not available."""
     if name in birthdays:
         print('{}\'s birthday is {}.'.format(name, birthdays[name]))
     else:
