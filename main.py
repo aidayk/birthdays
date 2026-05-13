acfd860c1f2fe8d4914b677049234abf49ebc3d0
Aiday <903774@stud.unive.it>
2026-05-13 13:59:29 +0200

    committing

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python3
 
 from birthdays import return_birthday
+from birthdays import add_birthday_entry
 
 return_birthday('Albert Einstein')
 return_birthday('Alan Turing')
+
+add_birthday_entry('Erling Haaland', '07/21/2000')
+add_birthday_entry('Albert Gore', '03/31/1948')
+add_birthday_entry('Elizabeth Bowes-Lyon', '08/04/1900')
