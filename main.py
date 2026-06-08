049d03028263afdc4b3699b1df65b4bed3a72d10
Aiday <903774@stud.unive.it>
2026-06-08 18:35:44 +0200

    use argparse

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 #! /usr/bin/env python3
 
-#from birthdays import return_birthday
-from birthdays import add_birthday_entry
-from birthdays import leap_year_birthdays
+import argparse
 
+from birthdays import birthdays
+from birthdays import return_birthday
 
+#from birthdays import leap_year_birthdays
+#add_birthday_entry('Erling Haaland', '07/21/2000')
+#add_birthday_entry('Albert Gore', '03/31/1948')
+#add_birthday_entry('Elizabeth Bowes-Lyon', '08/04/1900')
+#print(leap_year_birthdays())
 
-add_birthday_entry('Erling Haaland', '07/21/2000')
-add_birthday_entry('Albert Gore', '03/31/1948')
-add_birthday_entry('Elizabeth Bowes-Lyon', '08/04/1900')
+firstnames = []
+lastnames = []
 
-print(leap_year_birthdays())
+for name in birthdays:
+    firstname, lastname = name.split(maxsplit=1)
+    firstnames.append(firstname)
+    lastnames.append(lastname)
+
+parser = argparse.ArgumentParser(
+    description="Return the birthday of a person stored in the birthday dictionary."
+)
+
+parser.add_argument(
+    "--firstname",
+    required=True,
+    choices=firstnames,
+    help="Firstname of the person whose birthday is requested."
+)
+
+parser.add_argument(
+    "--lastname",
+    required=True,
+    choices=lastnames,
+    help="Lastname of the person whose birthday is requested."
+)
+
+args = parser.parse_args()
+
+full_name = args.firstname + " " + args.lastname
+
+return_birthday(full_name)
